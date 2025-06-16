49750ab8342017cc4b154504868b28ca65afc3fd
Spencer Banasik <smbanasik@gmail.com>
2025-06-16 16:15:00 -0500

    Mouse documentation

--- inc/gl_mouse.hpp
+++ inc/gl_mouse.hpp
@@ -31,26 +31,89 @@ class MouseContext {
     friend class gf::gl::GLManager;
 public:
 
+    /**
+    * @brief Constructor, sets mouse movement callback.
+    * @author Spencer Banasik
+    */
     MouseContext(Extent2D window_dims, GLFWwindow* window);
 
+    /**
+    * @brief Gets mouse coordinates.
+    * @author Spencer Banasik
+    * @returns The current coordinates of the mouse.
+    */
     Double2D get_mouse_coords();
+
+    /**
+    * @brief Gets the offset of the mouse with respect to to it's last coordinates.
+    * @author Spencer Banasik
+    * @returns The offset of the mouse.
+    */
     Double2D get_mouse_offset();
+
+    /**
+    * @brief Gets the velocity of it's mouse with respect to it's last coordinates.
+    * @author Spencer Banasik
+    * @details Divides the mouse offset by the time difference of the current and previous coordinates.
+    * Vector is then normalized.
+    * @returns The velocity of the mouse.
+    */
     Double2D get_mouse_velocity();
 
+
+    /**
+    * @brief Allows the user to add their own functionality to the mouse movement callback.
+    * @author Spencer Banasik
+    */
     void set_callback_mouse_movement(std::function<void(WInputContext* gl_context)>&& function) { callback_mouse_move = std::move(function); };
 
+    /**
+    * @brief Disables the mouse cursor, used for 3D camera.
+    * @author Spencer Banasik
+    */
     void disable_cursor();
+
+    /**
+    * @brief Enables the mouse cursor.
+    * @author Spencer Banasik
+    */
     void enable_cursor();
+
+    /**
+    * @brief Mouse cursor is restricted to the window?
+    * @author Spencer Banasik
+    */
     void enable_cursor_captured();
+
+    /**
+    * @brief Enables raw (unscaled and unaccelerated) mouse motion.
+    * @author Spencer Banasik
+    */
     void enable_raw_mouse();
+
+    /**
+    * @brief Disables raw mouse motion.
+    * @author Spencer Banasik
+    */
     void disable_raw_mouse();
 
     bool first_mouse = true;
 
 private:
 
+    /**
+    * @brief Callback for the glfw mouce movement.
+    * @author Spencer Banasik
+    * @details Sets the mouse times and mouse coordinates.
+    * @private
+    */
     static void callback_movement(GLFWwindow* window, double xpos, double ypos);
 
+    /**
+    * @brief Calls the user added callback.
+    * @author Spencer Banasik
+    * @private
+    */
     void call_movement();
 
     double prior_mouse_time;
